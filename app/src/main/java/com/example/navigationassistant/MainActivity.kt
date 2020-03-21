c2467a7dd6b56f6176dcbce091084c89530ae74e
dtaneli16 <dtaneli16@ku.edu.tr>
2020-03-21 14:40:47 +0300

    add yandex map

--- app/src/main/java/com/example/navigationassistant/MainActivity.kt
+++ app/src/main/java/com/example/navigationassistant/MainActivity.kt
@@ -1,12 +1,72 @@
 package com.example.navigationassistant
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.mapview.MapView
 
+
+/**
+ * This is a basic example that displays a map and sets camera focus on the target location.
+ * Note: When working on your projects, remember to request the required permissions.
+ */
 class MainActivity : AppCompatActivity() {
-
+    /**
+     * Replace "your_api_key" with a valid developer key.
+     * You can get it at the https://developer.tech.yandex.ru/ website.
+     */
+    private val MAPKIT_API_KEY = "64f7d05c-b2e2-48fd-b058-a7927cd25383"
+    private val TARGET_LOCATION = Point(41.091321, 29.061352)
+    private var mapView: MapView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        /**
+         * Set the api key before calling initialize on MapKitFactory.
+         * It is recommended to set api key in the Application.onCreate method,
+         * but here we do it in each activity to make examples isolated.
+         */
+        MapKitFactory.setApiKey(MAPKIT_API_KEY)
+        /**
+         * Initialize the library to load required native libraries.
+         * It is recommended to initialize the MapKit library in the Activity.onCreate method
+         * Initializing in the Application.onCreate method may lead to extra calls and increased battery use.
+         */
+        MapKitFactory.initialize(this)
+        // Now MapView can be created.
         setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+        mapView = findViewById<View>(R.id.mapview) as MapView
+
+        // And to show what can be done with it, we move the camera to the center of Saint Petersburg.
+        mapView!!.map.move(
+            CameraPosition(TARGET_LOCATION, 11.0f, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 5F),
+            null
+        )
+    }
+
+    override fun onStop() {
+        // Activity onStop call must be passed to both MapView and MapKit instance.
+        mapView!!.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+
+    override fun onStart() {
+        // Activity onStart call must be passed to both MapView and MapKit instance.
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        mapView!!.onStart()
     }
 }
+
+//class MainActivity : AppCompatActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+//}
